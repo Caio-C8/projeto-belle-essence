e5236d471c66a1311974c203fde0ec310b2110f9
Caio-C8 <caiomgf50@gmail.com>
2025-02-26 13:57:34 -0300

    iniciando o projeto. estrutura de pastas, header e footer feitos

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaRegImage } from "react-icons/fa6";
+import { IoLogoInstagram } from "react-icons/io5";
+import { IoLogoFacebook } from "react-icons/io5";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <div className="footer__logo">
+          <FaRegImage className="footer__logo__img" />
+        </div>
+
+        <div className="footer__social">
+          <p className="footer__social__title">Redes Sociais</p>
+
+          <div className="footer__social__icons">
+            <IoLogoInstagram className="footer__social__icon" />
+            <IoLogoFacebook className="footer__social__icon" />
+          </div>
+        </div>
+      </div>
+
+      <div className="footer__address">
+        <p>Rua Romualdo Mendonça, 805 - João Pineiro, MG - (38) 98801-6588</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
