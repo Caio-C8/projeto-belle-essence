e00943537ea9bfee69efd686db54559fa0362be6
Caio-C8 <caiomgf50@gmail.com>
2025-03-08 14:09:03 -0300

    início da organização das rotas, criação das pastas das págians

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Search from "./pages/Search/Search";
+import Cart from "./pages/Cart/Cart";
+import Favorites from "./pages/Favorites/Favorites";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <main></main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
