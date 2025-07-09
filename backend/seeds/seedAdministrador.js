117acf561da14a563c1408919746b4354002e19d
Caio-C8 <caiomgf50@gmail.com>
2025-07-09 12:15:43 -0300

    chore: cria novos seeds e altera alguns já existentes

---
+++ backend/seeds/seedAdministrador.js
@@ -0,0 +1,31 @@
+const pool = require("../connect");
+const bcrypt = require("bcrypt");
+
+const senha = "123";
+const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+const admnistradores = [
+  {
+    email: "adm@email.com",
+    senha: senhaCriptografada,
+  },
+];
+
+async function seedAdministrador() {
+  for (const adm of admnistradores) {
+    try {
+      await pool.query(
+        "INSERT INTO administradores (email_adm, senha_adm) VALUES ($1, $2)",
+        [adm.email, adm.senha]
+      );
+      console.log(`✅ Administrador ${adm.email} inserido com sucesso!`);
+    } catch (err) {
+      console.error(`❌ Erro ao inserir administrador ${adm.email}:`, err);
+    }
+  }
+
+  console.log("🌱 Inserção concluída.");
+  process.exit();
+}
+
+seedAdministrador();
